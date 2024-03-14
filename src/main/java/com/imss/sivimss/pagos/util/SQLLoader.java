4b37907464ea206bc3eb85698a9217c6f3ab54cd
galo14 <yo123garcia@gmail.com>
2024-03-14 13:41:37 -0600

    servicio de bitacora

---
+++ src/main/java/com/imss/sivimss/pagos/util/SQLLoader.java
@@ -0,0 +1,41 @@
+package com.imss.sivimss.pagos.util;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import lombok.Data;
+
+@Component
+@Data
+public class SQLLoader {
+
+    private final ResourceLoader resourceLoader;
+
+    private String bitacoraNuevoRegistro;
+    private String seleccionarPago;
+
+    public SQLLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:sql/BITACORA_CREAR_REGISTRO.SQL");
+        byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+        bitacoraNuevoRegistro = new String(bytes, StandardCharsets.UTF_8);
+
+        resource = resourceLoader.getResource("classpath:sql/PAGOS_CONSULTA_REGISTRO.SQL");
+        bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+        bitacoraNuevoRegistro = new String(bytes, StandardCharsets.UTF_8);
+
+    }
+
+    
+}
